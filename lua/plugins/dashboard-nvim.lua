02c87f93c51c4db66cc7feca825cef1fe86a2c77
pikajian <pikajian@actions-micro.com>
2023-11-15 16:03:08 +0800

    Update dashboard-nvim.

--- lua/plugins/dashboard-nvim.lua
+++ lua/plugins/dashboard-nvim.lua
@@ -1,3 +1,13 @@
-require('dashboard').setup {
-    -- config
+local opts = {
+	theme = "doom",
+	hide = {
+		-- this is taken care of by lualine
+		-- enabling this messes up the actual laststatus setting after loading a file
+		statusline = false,
+	},
 }
+
+require("dashboard").setup({
+    opts
+	-- config
+})
