4c078112ba39087499c77a1130a63513cfe5841d
pikajian <pikajian@actions-micro.com>
2024-03-27 15:25:26 +0800

    Enable Copilot and merge ui plugins to ui.lua.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,4 +1,72 @@
 return {
+  {'nvim-tree/nvim-web-devicons'},
+  {
+    'rcarriga/nvim-notify',
+    opts = {
+      -- "fade", "slide", "fade_in_slide_out", "static"
+      stages = "static",
+      timeout = 1000
+    }
+  },
+  {'MunifTanjim/nui.nvim'},
+  {'stevearc/dressing.nvim'},
+  {
+    'folke/noice.nvim',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify'
+    },
+    opts = {
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
+
+    }
+  },
+
+  {
+    "akinsho/bufferline.nvim",
+    dependencies = {'nvim-tree/nvim-web-devicons'},
+    event = "VeryLazy",
+    keys = {
+      { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+      { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+      { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+      { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+    },
+    opts = {
+      options = {
+          separator_style = "thin",
+          diagnostics = "nvim_lsp",
+          diagnostics_indicator = function(_, _, diag)
+              local diagnostics = require("utils.ui").icons.diagnostics
+              local ret = (diag.error and diagnostics.Error .. diag.error .. " " or "")
+                  .. (diag.warning and diagnostics.Warn .. diag.warning or "")
+              return vim.trim(ret)
+          end,
+          --[[diagnostics_indicator = function(count, level, diagnostics_dict, context)
+               local icon = level:match("error") and " " or " "
+               return " " .. icon .. count
+           end,]]
+      },
+    },
+    config = function(_, opts)
+      require("bufferline").setup(opts)
+      -- Fix bufferline when restoring a session
+      vim.api.nvim_create_autocmd("BufAdd", {
+        callback = function()
+          vim.schedule(function()
+            pcall(nvim_bufferline)
+          end)
+        end,
+      })
+    end,
+  },
+
   {
     "lukas-reineke/indent-blankline.nvim",
     event = "LazyFile",
